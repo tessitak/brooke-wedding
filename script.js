fac29526ce935facbac82a485f97a566559ca5ba
tessitak <79184153+tessitak@users.noreply.github.com>
2023-02-05 17:58:18 -0800

    added all pages

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function insertNav() {
 			<li class="nav-item"><a href="index.html">Home</a></li>
 			<li class="nav-item"><a href="details.html">Details</a></li>
 			<li class="nav-item"><a href="travel.html">Travel</a></li>
-			<li class="nav-item"><a href="gallery.html">Gallery</a></li>
 			<li class="nav-item"><a href="faq.html">FAQ</a></li>
 			<li class="nav-item"><a href="weddingparty.html">Wedding Party</a></li>
+			<li class="nav-item"><a href="gallery.html">Gallery</a></li>
 		</ul>
 		<div class="hamburger hidden" onclick="burgerToggle(this)">
 		  <div class="bar1"></div>
@@ -22,9 +22,9 @@ function insertNav() {
 			<li class="side-item"><a href="index.html">Home</a></li>
 			<li class="side-item"><a href="details.html">Details</a></li>
 			<li class="side-item"><a href="travel.html">Travel</a></li>
-			<li class="side-item"><a href="gallery.html">Photos</a></li>
 			<li class="side-item"><a href="faq.html">FAQ</a></li>
 			<li class="side-item"><a href="weddingparty.html">Wedding Party</a></li>
+			<li class="side-item"><a href="gallery.html">Photos</a></li>
 		</ul>
 	</aside>`;
 
